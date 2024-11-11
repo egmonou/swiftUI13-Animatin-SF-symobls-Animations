4e3d6e1dd1dd1f35b6fc3bc3fe386625b79ee3c9
monour <mohamednour25@gmail.com>
2024-11-11 15:36:57 +0400

    Initial Commit

---
+++ SF symobls AnimationsTests/SF_symobls_AnimationsTests.swift
@@ -0,0 +1,17 @@
+//
+//  SF_symobls_AnimationsTests.swift
+//  SF symobls AnimationsTests
+//
+//  Created by Mohamed Nour on 11/11/2024.
+//
+
+import Testing
+@testable import SF_symobls_Animations
+
+struct SF_symobls_AnimationsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
